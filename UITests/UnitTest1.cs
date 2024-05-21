2673a0e7ee1c7ce80660036e89ec83bca5cbd7ee
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-05-21 14:09:05 -0400

    partial unit test1

--- UITests/UnitTest1.cs
+++ UITests/UnitTest1.cs
@@ -12,6 +12,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
+using System.Text.RegularExpressions;
+using System.Text;
 
 
 namespace UITest
@@ -44,7 +46,42 @@ namespace UITest
             }
         }
 
-        
+        // Create a test method 'EmployeeIsHourlyEmployee_ShouldSucceed' to assert that the employee is an hourly employee
+        // The test should check that the element with id of 'caption_typeof_employee' contains the text 'Hourly Employee'
+
+        [TestMethod]
+        public void EmployeeIsHourlyEmployee_ShouldSucceed()
+        {
+            _driver?.Navigate().GoToUrl("https://localhost:7088/Employee/Details/4");
+            var caption = _driver?.FindElement(By.Id("caption_typeof_employee"));
+            if (caption != null)
+            {
+                Assert.IsTrue(caption.Text.Contains("Hourly Employee"));
+            }
+            else
+            {
+                Assert.Fail("Element with id 'caption_typeof_employee' not found");
+            }
+        }
+
+        // Create a test method 'PayIsCurrencyValue_ShouldSucceed' to assert that the element 'pay' is a currency value
+        [TestMethod]
+        public void PayIsCurrencyValue_ShouldSucceed()
+        {
+            _driver?.Navigate().GoToUrl("https://localhost:7088/Employee/Details/4");
+            var pay = _driver?.FindElement(By.Id("pay"));
+            if (pay != null)
+            {
+                Assert.IsTrue(Regex.IsMatch(pay.Text, @"^\$?\d{1,3}(,?\d{3})*(\.\d{2})?$"));
+            }
+            else
+            {
+                Assert.Fail("Element with id 'pay' not found");
+            }
+        }
+
+
+
         [TestCleanup]
         public void TestCleanup()
         {
