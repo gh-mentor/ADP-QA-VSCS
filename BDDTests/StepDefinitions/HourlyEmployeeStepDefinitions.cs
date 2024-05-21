08d3bd2179125592ee51c38df2f7e3b22e70fabf
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-05-21 13:33:59 -0400

    completed step definitions

--- BDDTests/StepDefinitions/HourlyEmployeeStepDefinitions.cs
+++ BDDTests/StepDefinitions/HourlyEmployeeStepDefinitions.cs
@@ -17,6 +17,18 @@ namespace BDDTests.StepDefinitions
         private double? _payment;
         private string? _details;
 
+        /*
+         * Create a method named 'GivenTheHourlyRateIs10'.
+         * Details:
+         * This method creates an instance of the 'HourlyEmployee' class with the hourly rate set to 10.
+         * All constructor arguments except the hourly rate and hours worked are nullable and should be set to 'null'.
+         */
+        [Given(@"the hourly rate is 10")]
+        public void GivenTheHourlyRateIs10()
+        {
+            _employee = new HourlyEmployee(null, null, null, null, null, null, 10, 0);
+        }
+
 
 
     }
